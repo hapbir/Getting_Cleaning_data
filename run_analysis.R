45dfc1661a3da562fd14109c004caa3894279e3a
hapbir <hphu79@gmail.com>
2014-09-20 17:29:11 +0800

    answer for project

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
 # 1.  Merges the training and the test sets to create one data set.
 
-train <- read.table("X_train.txt",header=FALSE)
-test <- read.table("X_test.txt",header=FALSE)
+train <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt",header=FALSE)
+test <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/testX_test.txt",header=FALSE)
 data_complete <- rbind(train, test)                               # raw data
 
 # 4.  Appropriately labels the data set with descriptive variable names. 
 
-feature_list <- read.table("features.txt",header=FALSE)[,2]
+feature_list <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt",header=FALSE)[,2]
 names(data_complete) <- feature_list
 
 # 2.  Extracts only the measurements on the mean and standard deviation for each measurement. 
@@ -17,12 +17,12 @@ data_filtered <- data_complete[,hits]
 # 3.  Uses descriptive activity names to name the activities in the data set
 
 # get activity label- numeric
-train_label <- read.table("Y_train.txt",header=FALSE)
-test_label <- read.table("Y_test.txt",header=FALSE)
+train_label <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/Y_train.txt",header=FALSE)
+test_label <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/Y_test.txt",header=FALSE)
 activity_label <- c(train_label[[1]],test_label[[1]])                  
 
 # get activity name - string
-act_table <- read.table("activity_labels.txt",header=FALSE)
+act_table <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt",header=FALSE)
 act_table[,2] <- as.character(act_table[,2])                           
 
 # maping the activity label to activity name
@@ -36,8 +36,8 @@ data_named <- cbind(activity_name=factor(act_lst), data_filtered)
 
 # assign object ID
 
-objID_train <- read.table("subject_train.txt",header =FALSE)
-objID_test <- read.table("subject_test.txt",header=FALSE)
+objID_train <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt",header =FALSE)
+objID_test <- read.table("getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt",header=FALSE)
 objID <- c(objID_train[[1]],objID_test[[1]])
 
 data_final <- cbind(objID = factor(objID), data_named)
